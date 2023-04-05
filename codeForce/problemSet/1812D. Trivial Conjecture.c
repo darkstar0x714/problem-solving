5660e5dccd01f888cb291ec684a8cc292bd392ed
Amir W. Fathy <108402930+darkstar0x714@users.noreply.github.com>
2023-04-05 20:29:03 +0200

    5/4/2023

---
+++ codeForce/problemSet/1812D. Trivial Conjecture.c
@@ -0,0 +1,15 @@
+/**
+ * @ Author: Amir W. Fathy
+ * @ Create Time: 2023-04-05 20:06:35
+ * @ Known as: DarkStar0x714
+ * @ link:  https://codeforces.com/contest/1812/problem/D
+ * @ tags:  math - constructive algorithms - *special problem -number theory
+ * @ Problem Level:  *1000
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("0");
+}
