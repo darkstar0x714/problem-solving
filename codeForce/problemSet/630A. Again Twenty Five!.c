759691dd2950ed17101a8651e7ef11d35099401e
Amir W. Fathy <108402930+darkstar0x714@users.noreply.github.com>
2023-04-11 20:28:45 +0200

    11/4/2023

---
+++ codeForce/problemSet/630A. Again Twenty Five!.c
@@ -0,0 +1,16 @@
+/**
+ * @ Author: Amir W. Fathy
+ * @ Create Time: 2023-04-11 20:26:02
+ * @ Known as: DarkStar0x714
+ * @ link:  https://codeforces.com/problemset/problem/630/A
+ * @ tags:  number theory
+ * @ Problem Level:  *800
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("25\n");
+    return 0;
+}
