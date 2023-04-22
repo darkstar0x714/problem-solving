5376be81098d7c6c0831b7ff1fbdae4a7ac32371
Amir W. Fathy <108402930+darkstar0x714@users.noreply.github.com>
2023-04-22 18:01:57 +0200

    22/4/2023

---
+++ codeForce/problemSet/1285A. Mezo Playing Zoma.c
@@ -0,0 +1,17 @@
+/**
+ * @ Author: Amir W. Fathy
+ * @ Create Time: 2023-04-22 17:59:28
+ * @ Known as: DarkStar0x714
+ * @ link:  https://codeforces.com/problemset/problem/1285/A
+ * @ tags:  math - implementation
+ * @ Problem Level:  *800
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    long long numberOfMovements;
+    scanf("%lld", &numberOfMovements);
+    printf("%lld", numberOfMovements + 1);
+}
