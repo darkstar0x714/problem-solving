9ce21e8261c670efbd7a98a1a29f1d9c19bb518d
Amir W. Fathy <108402930+darkstar0x714@users.noreply.github.com>
2023-10-13 18:59:44 +0300

    3 Months Preparation Kit : week 2 : day 4

---
+++ HackerRank/3 Months Preparation Kit/week 2/D4_Diagonal Difference.cpp
@@ -0,0 +1,112 @@
+/**
+ * @ Author: Amir W. Fathy
+ * @ Create Time: 2023-10-13 18:51:57
+ * @ As Known As: darkstar0x714
+ * @ link:
+ * https://www.hackerrank.com/challenges/three-month-preparation-kit-diagonal-difference/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+int diagonalDifference(vector<vector<int>> arr)
+{
+    int mainDialog = 0, secondaryDialog = 0;
+    int arrSize = arr.size();
+    for (int i = 0; i < arrSize; i++)
+    {
+        mainDialog = mainDialog + arr[i][i];
+        secondaryDialog = secondaryDialog + arr[i][arrSize - 1 - i];
+    }
+    return abs((mainDialog - secondaryDialog));
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<vector<int>> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i].resize(n);
+
+        string arr_row_temp_temp;
+        getline(cin, arr_row_temp_temp);
+
+        vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
+
+        for (int j = 0; j < n; j++)
+        {
+            int arr_row_item = stoi(arr_row_temp[j]);
+
+            arr[i][j] = arr_row_item;
+        }
+    }
+
+    int result = diagonalDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
+
+vector<string> split(const string &str)
+{
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos)
+    {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
